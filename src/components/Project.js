d2db12fd758a1bc16d4d72ed643b6c7b57ce56da
Rushikesh Wani <wanirushikeshanil11@gmail.com>
2024-05-21 11:25:19 +0530

    Adjusted the UI of project section

--- src/components/Project.js
+++ src/components/Project.js
@@ -6,9 +6,9 @@ const Project = () => {
       <h1 className="text-3xl text-gray-300 font-medium py-2 sm:py-4 text-center">
         Projects
       </h1>
-      <div className="cards-container mx-auto flex flex-wrap md:flex-nowrap gap-x-2 gap-y-2 sm:gap-x-5 sm:gap-y-5 justify-center align-middle items-center">
-        <div className="Card w-auto h-auto sm:w-80 rounded-xl hover:shadow-md hover:shadow-neutral-500/50">
-          <div className="Card-img h-36">
+      <div className="cards-container mx-auto flex flex-wrap gap-x-2 gap-y-6 sm:gap-x-5 sm:gap-y-5 justify-center align-middle items-center">
+        <div className="Card w-auto h-auto sm:w-80 md:w-[350px] rounded-xl duration-500 hover:shadow-md hover:shadow-slate-400/30">
+          <div className="Card-img h-44">
             <img
               className="w-full h-full object-cover rounded-tr-lg rounded-tl-lg"
               src="https://stastic.ui4free.com/public/images/project-thumbnail-figma-design_1646617617.jpg"
@@ -20,11 +20,10 @@ const Project = () => {
               <p className="bg-slate-400 px-3 rounded-xl text-black">
                 MERN Stack
               </p>
-              <p className="bg-slate-400 px-3 rounded-xl text-black">
-                React JS
-              </p>
             </div>
-            <p className="text-xl font-medium">MERN CRUD Application</p>
+            <p className="text-xl font-medium uppercase">
+              MERN CRUD Application
+            </p>
             <p className="text-gray-300 text-sm">
               lorem shdh fsdh ipsum text sbgdgs jsywedd sdgs gdgsshs sgagdsg
               agsa ueyweb yy dh dab afgtv atg sds d ashd hsd ahj ddnd nnsa sasa
@@ -40,8 +39,8 @@ const Project = () => {
             </div>
           </div>
         </div>
-        <div className="Card w-auto h-auto sm:w-80 rounded-xl hover:shadow-md hover:shadow-neutral-500/50">
-          <div className="Card-img h-36">
+        <div className="Card w-auto h-auto sm:w-80 md:w-[350px] rounded-xl duration-500 hover:shadow-md hover:shadow-slate-400/30">
+          <div className="Card-img h-44">
             <img
               className="w-full h-full object-cover rounded-tr-lg rounded-tl-lg"
               src="https://s3-alpha.figma.com/hub/file/1472262063/f728c543-95e1-466b-8699-acabb5e96c93-cover.png"
@@ -51,13 +50,12 @@ const Project = () => {
           <div className="py-3 px-3 bg-slate-900 rounded-bl-lg rounded-br-lg">
             <div className="pb-1 flex text-xs sm:text-sm text-nowrap flex-wrap gap-x-2 gap-y-1">
               <p className="bg-slate-400 px-3 rounded-xl text-black">
-                MERN Stack
-              </p>
-              <p className="bg-slate-400 px-3 rounded-xl text-black">
                 React JS
               </p>
             </div>
-            <p className="text-xl font-medium">MERN CRUD Application</p>
+            <p className="text-xl font-medium uppercase">
+              Food-Villa Food Ordering App
+            </p>
             <p className="text-gray-300 text-sm">
               lorem shdh fsdh ipsum text sbgdgs jsywedd sdgs gdgsshs sgagdsg
               agsa ueyweb yy dh dab afgtv atg sds d ashd hsd ahj ddnd nnsa sasa
@@ -73,8 +71,8 @@ const Project = () => {
             </div>
           </div>
         </div>
-        <div className="Card w-auto h-auto sm:w-80 rounded-xl hover:shadow-md hover:shadow-neutral-500/50">
-          <div className="Card-img h-36">
+        <div className="Card w-auto h-auto sm:w-80 md:w-[350px] rounded-xl duration-500 hover:shadow-md hover:shadow-slate-400/30">
+          <div className="Card-img h-44">
             <img
               className="w-full h-full object-cover rounded-tr-lg rounded-tl-lg"
               src="https://s3-alpha.figma.com/hub/file/3911311858/297285c6-e724-45d7-95ec-e364206fdd9d-cover.png"
@@ -84,13 +82,10 @@ const Project = () => {
           <div className="py-3 px-3 bg-slate-900 rounded-bl-lg rounded-br-lg">
             <div className="pb-1 flex text-xs sm:text-sm text-nowrap flex-wrap gap-x-2 gap-y-1">
               <p className="bg-slate-400 px-3 rounded-xl text-black">
-                MERN Stack
-              </p>
-              <p className="bg-slate-400 px-3 rounded-xl text-black">
                 React JS
               </p>
             </div>
-            <p className="text-xl font-medium">MERN CRUD Application</p>
+            <p className="text-xl font-medium uppercase">YouTube Clone</p>
             <p className="text-gray-300 text-sm">
               lorem shdh fsdh ipsum text sbgdgs jsywedd sdgs gdgsshs sgagdsg
               agsa ueyweb yy dh dab afgtv atg sds d ashd hsd ahj ddnd nnsa sasa
@@ -106,8 +101,8 @@ const Project = () => {
             </div>
           </div>
         </div>
-        <div className="Card w-auto h-auto sm:w-80 rounded-xl hover:shadow-md hover:shadow-neutral-500/50">
-          <div className="Card-img h-36">
+        <div className="Card w-auto h-auto sm:w-80 md:w-[350px] rounded-xl duration-500 hover:shadow-md hover:shadow-slate-400/30">
+          <div className="Card-img h-44">
             <img
               className="w-full h-full object-cover rounded-tr-lg rounded-tl-lg"
               src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_KalHLLRTwO-LM1f7AhkLED9AUN3T4Tp1rnIiAPvYN-UVKl-CH6gfezY2MS2-3dK2BSw&usqp=CAU"
@@ -123,7 +118,9 @@ const Project = () => {
                 React JS
               </p>
             </div>
-            <p className="text-xl font-medium">MERN CRUD Application</p>
+            <p className="text-xl font-medium uppercase">
+              My Portfolio Website
+            </p>
             <p className="text-gray-300 text-sm">
               lorem shdh fsdh ipsum text sbgdgs jsywedd sdgs gdgsshs sgagdsg
               agsa ueyweb yy dh dab afgtv atg sds d ashd hsd ahj ddnd nnsa sasa
